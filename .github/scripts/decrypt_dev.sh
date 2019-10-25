54e97c659ae336f41b574b8a488dd680b0cdb52f
Bill Hefty <billhefty@gmail.com>
2019-10-25 11:45:07 -0500

    update path

--- .github/scripts/decrypt_dev.sh
+++ .github/scripts/decrypt_dev.sh
@@ -4,4 +4,4 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$DEV_CONFIG_PASSPHRASE" \
---output $HOME/secrets/config.dev.json config.dev.json.gpg
+--output $HOME/secrets/config.dev.json client/config.dev.json.gpg
