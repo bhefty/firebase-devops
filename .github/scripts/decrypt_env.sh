bebc6b7bd0433487e7d929d794ca6d28cbd0a753
Bill Hefty <billhefty@gmail.com>
2019-10-25 15:42:34 -0500

    Make decrypt env independent

--- .github/scripts/decrypt_env.sh
+++ .github/scripts/decrypt_env.sh
@@ -3,4 +3,4 @@
 # Decrypt the file
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$ENV_PASSPHRASE" \
---output client/.env client/.env.gpg
+--output client/.env client/.$ENV_FILE_NAME.gpg
