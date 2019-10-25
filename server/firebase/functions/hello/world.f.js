49326edbab8a71577db770c41fd4657c80aa06b6
Bill Hefty <billhefty@gmail.com>
2019-10-25 18:50:25 -0500

    add func

---
+++ server/firebase/functions/hello/world.f.js
@@ -0,0 +1,24 @@
+// INCLUDE BELOW AT TOP OF ALL FUNCTION FILES
+const functions = require(`firebase-functions`);
+const admin = require(`firebase-admin`);
+try {
+  admin.initializeApp(functions.config().firebase);
+} catch (e) {
+  /***/
+}
+try {
+  const settings = { timestampsInSnapshots: true };
+  admin.firestore().settings(settings);
+} catch (e) {
+  /***/
+}
+// INCLUDE ABOVE AT TOP OF ALL FUNCTION FILES
+
+exports = module.exports = functions.https.onCall(async (data, context) => {
+  try {
+    return { message: `Hello world!` };
+  } catch (error) {
+    console.error(`Error`, error);
+    return { error };
+  }
+});
