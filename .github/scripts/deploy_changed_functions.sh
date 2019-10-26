cb6e12b3fa21f2174d8f6af6a56a2486e160b462
Bill Hefty <billhefty@gmail.com>
2019-10-25 23:00:02 -0500

    echo the script

---
+++ .github/scripts/deploy_changed_functions.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+git diff --name-only origin/$SOURCE_BRANCH server-init |
+grep '\.f.\js$' |
+for f in $(cut -d'/' -f4-); do
+  functionName=$(echo ${f%?????} | gsed -r 's/(\/)([a-z])/\U\2/g')
+  echo $functionName
+  if [ -z ${DEPLOY_FUNCTIONS+x} ]; then DEPLOY_FUNCTIONS=functions:$functionName; else DEPLOY_FUNCTIONS=$DEPLOY_FUNCTIONS,functions:$functionName; fi
+  # firebase deploy --only functions:${functionName}
+done
--- server/firebase/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for f in $(diff -Nqr functions/ functions-staging/ | awk '{print $4}' | cut -d'/' -f2-); do
-  functionName=$(echo ${f%?????} | gsed -r 's/(\/)([a-z])/\U\2/g')
-  echo $functionName
-  # firebase deploy --only functions:${functionName}
-done
