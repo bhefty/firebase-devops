8aa6568cedf15d38df3b8a85905f05c8fa959252
Bill Hefty <billhefty@gmail.com>
2019-10-25 23:02:51 -0500

    use sed

--- .github/scripts/deploy_changed_functions.sh
+++ .github/scripts/deploy_changed_functions.sh
@@ -3,7 +3,7 @@
 git diff --name-only origin/$SOURCE_BRANCH $CURRENT_COMMIT |
 grep '\.f.\js$' |
 for f in $(cut -d'/' -f4-); do
-  functionName=$(echo ${f%?????} | gsed -r 's/(\/)([a-z])/\U\2/g')
+  functionName=$(echo ${f%?????} | sed -r 's/(\/)([a-z])/\U\2/g')
   echo $functionName
   if [ -z ${DEPLOY_FUNCTIONS+x} ]; then DEPLOY_FUNCTIONS=functions:$functionName; else DEPLOY_FUNCTIONS=$DEPLOY_FUNCTIONS,functions:$functionName; fi
   # firebase deploy --only functions:${functionName}
