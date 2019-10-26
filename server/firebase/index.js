65a6f4636efa42b91c71ee69e8bdd660f6337777
Bill Hefty <billhefty@gmail.com>
2019-10-26 00:11:51 -0500

    comment

--- server/firebase/index.js
+++ server/firebase/index.js
@@ -27,6 +27,7 @@ const admin = require(`firebase-admin`);
   // credential: admin.credential.cert(serviceAccount),
   // databaseURL: fbConfig.db,
 // });
+// Just a comment
 admin.initializeApp()
 
 const glob = require(`glob`);
