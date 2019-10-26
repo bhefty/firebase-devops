dfc131bfb5a95b2974148be28e4ee8cf3f0c8a56
Bill Hefty <billhefty@gmail.com>
2019-10-25 23:43:11 -0500

    echo test

--- .github/scripts/deploy_changed_functions.sh
+++ .github/scripts/deploy_changed_functions.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
-git diff --name-only origin/$SOURCE_BRANCH $CURRENT_COMMIT |
-grep '\.f.\js$' |
-for f in $(cut -d'/' -f4-); do
+# Get a list of changed functions (ending in `.f.js`)
+DIFFS=$(git diff --name-only origin/$SOURCE_BRANCH $CURRENT_COMMIT | grep '\.f.\js$')
+
+# For each function, get the camelcased name (hello/world.f.js becomes helloWorld)
+for f in $(echo "${DIFFS}" | cut -d'/' -f4-); do
   functionName=$(echo ${f%?????} | sed -r 's/(\/)([a-z])/\U\2/g')
-  echo $functionName
-  if [ -z ${DEPLOY_FUNCTIONS+x} ]; then DEPLOY_FUNCTIONS=functions:$functionName; else DEPLOY_FUNCTIONS=$DEPLOY_FUNCTIONS,functions:$functionName; fi
-  # firebase deploy --only functions:${functionName}
+
+  # Check if FUNCTIONS_LIST has a value
+  if [ -z ${FUNCTIONS_LIST+x} ];
+    # Set the first `functions:` statement
+    then FUNCTIONS_LIST=functions:$functionName;
+    # Append the `,functions:` statement
+    else FUNCTIONS_LIST=$FUNCTIONS_LIST,functions:$functionName;
+  fi
 done
+
+# Print the final list of functions to be deployed
+echo $FUNCTIONS_LIST
