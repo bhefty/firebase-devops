92c382108000c09c93c38455f54cd66d89c66641
Bill Hefty <billhefty@gmail.com>
2019-10-28 09:39:21 -0500

    testing script

--- .github/scripts/did_server_change.sh
+++ .github/scripts/did_server_change.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# Get a list of changed files (starting with `client/`)
+# Get a list of changed files (starting with `server/`)
 DIFF_FILES=$(git diff --name-only origin/$SOURCE_BRANCH $CURRENT_COMMIT |
-  grep '^client\/' |
+  grep '^server\/' |
   grep -v '\.gitignore$'
 )
 
 if [ -n "$DIFF_FILES" ];
-  # Return "true" - changes detected in `client/`
+  # Return "true" - changes detected in `server/`
   then echo true
-  # Return "false" - no changes in `client/`
+  # Return "false" - no changes in `server/`
   else echo 'false'
 fi
